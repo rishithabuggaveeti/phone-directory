e1c484a1a659433825471b94015054051e672169
rishithabuggaveeti <rishitha768@gmail.com>
2023-04-11 12:30:35 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-
-
 function App() {
   return (
-    <div >
-      phone directory
+    <div>
+      {/* <div>phone directory</div>
+      <button>Add</button>
+      <div>
+        <span>Name</span> <br />
+        <span>Phone</span>
+      </div> */}
+      <label htmlFor="name">Name:</label>
+      <input id="name" type="text" placeholder="Type Here" defaultvalue="Rishitha"/>
     </div>
   );
 }
