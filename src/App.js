20d9185eebce3027ad8af4daf3a43b50c1be6876
rishithabuggaveeti <rishitha768@gmail.com>
2023-04-14 15:19:00 +0530

    Created 'Header' Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
+import React from 'react';
+import Header from "./Header";
 function App() {
   return (
     <div>
-      {/* <div>phone directory</div>
+      <div>phone directory</div>
       <button>Add</button>
       <div>
         <span>Name</span> <br />
         <span>Phone</span>
-      </div> */}
-      <label htmlFor="name">Name:</label>
-      <input id="name" type="text" placeholder="Type Here" defaultvalue="Rishitha"/>
+      </div> 
+      
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const Header = function(){
+    return(
+        <div className='header'>
+            phone directory
+        </div>
+
+    )
+}
+export default Header;
